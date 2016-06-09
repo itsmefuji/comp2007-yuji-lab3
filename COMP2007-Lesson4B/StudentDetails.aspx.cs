826164ace000d0624bbd7269f00cbb033ce2043e
Yuji Fujiyama <yuji.0321.fujiyama@gmail.com>
2016-06-09 16:05:25 -0400

    Add project files.

---
+++ COMP2007-Lesson4B/StudentDetails.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+// required using statements to access EF DB 
+using COMP2007_Lesson4B.Models;
+using System.Web.ModelBinding;
+
+namespace COMP2007_Lesson4B
+{
+    public partial class StudentDetails : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if ((!IsPostBack) && (Request.QueryString.Count > 0))
+            {
+                this.GetStudent();
+            }
+        }
+
+        protected void GetStudent()
+        {
+            // populate the form with existing student record
+            int StudentID = Convert.ToInt32(Request.QueryString["StudentID"]);
+
+            // connect the DB with EF
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                // populate a student instance with the StudentID from the URL parameter
+                Student updateStudent = (from student in db.Students
+                                         where student.StudentID == StudentID
+                                         select student).FirstOrDefault();
+
+                // map the student properties to the form controls
+                if (updateStudent != null)
+                {
+                    LastNameTextBox.Text = updateStudent.LastName;
+                    FirstNameTextBox.Text = updateStudent.FirstMidName;
+                    EnrollmentDateTextBox.Text = updateStudent.EnrollmentDate.ToString("yyyy-MM-dd");
+                }
+            }
+        }
+
+        protected void CancelButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Students.aspx");
+        }
+
+        protected void SaveButton_Click(object sender, EventArgs e)
+        {
+            // connect to EF DB
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                // use the student model to save a new record
+                Student newStudent = new Student();
+
+                int StudentID = 0; 
+
+                if(Request.QueryString.Count > 0)
+                {
+                    //get the id from URL 
+                    StudentID = Convert.ToInt32(Request.QueryString["StudentID"]);
+
+                    //get the current student from the EF DB
+                    newStudent = (from student in db.Students
+                                  where student.StudentID == StudentID
+                                  select student).FirstOrDefault(); 
+                }
+
+                //Add form data to the new student record
+                newStudent.LastName = LastNameTextBox.Text;
+                newStudent.FirstMidName = FirstNameTextBox.Text;
+                newStudent.EnrollmentDate = Convert.ToDateTime(EnrollmentDateTextBox.Text);
+
+                // check to see if a new stuent is being added
+                if(StudentID == 0)
+                {
+                    db.Students.Add(newStudent); 
+                }
+
+                // run insert in DB
+                db.SaveChanges();
+
+                // redirect to the updated students page
+                Response.Redirect("~/Students.aspx");
+            }
+        }
+    }
+}
---
+++ COMP2007-Lesson4B/Students.aspx.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using COMP2007_Lesson4B.Models;
+using System.Web.ModelBinding;
+
+
+namespace COMP2007_Lesson4B
+{
+    public partial class Students : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // if loading the page for the first time, populate the student grid
+            if (!IsPostBack)
+            {
+                // create two session variables to hold our sorting data
+                Session["SortColumn"] = "StudentID";
+                Session["SortDirection"] = "ASC";
+
+                // Get the student data
+                this.GetStudents();
+            }
+        }
+
+        protected void GetStudents()
+        {
+            // connect to EF
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                // create a query string to add to the LINQ Query
+                string SortString = Session["SortColumn"].ToString() + " " + Session["SortDirection"].ToString();
+
+                // query the Students Table using EF and LINQ
+                var Students = (from allStudents in db.Students
+                                select new
+                                {
+                                    allStudents.StudentID,
+                                    allStudents.LastName,
+                                    allStudents.FirstMidName,
+                                    allStudents.EnrollmentDate
+                                });
+
+                //bind the result to the GridView
+                StudentsGridView.DataSource = Students.ToList();
+                StudentsGridView.DataBind();
+            }
+        }
+
+        protected void StudentsGridView_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            // store which row was clicked
+            int selectedRow = e.RowIndex;
+
+            // get the selected StudentID using the Grid's DataKey Collection
+            int StudentID = Convert.ToInt32(StudentsGridView.DataKeys[selectedRow].Values["StudentID"]);
+
+            // use EF to find the selected student from DB and remove it
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                Student deletedStudent = (from studentRecords in db.Students
+                                          where studentRecords.StudentID == StudentID
+                                          select studentRecords).FirstOrDefault();
+
+                // perform the removal in the DB
+                db.Students.Remove(deletedStudent);
+                db.SaveChanges();
+
+                // refresh the grid
+                this.GetStudents();
+
+            }
+        }
+
+        protected void StudentsGridView_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            // Set the new page number
+            StudentsGridView.PageIndex = e.NewPageIndex;
+
+            // refresh the grid
+            this.GetStudents();
+        }
+
+        protected void StudentsDropDownList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // set new Page size
+            StudentsGridView.PageSize = Convert.ToInt32(StudentsDropDownList.SelectedValue);
+
+            // refresh the grid
+            this.GetStudents();
+        }
+
+        protected void StudentsGridView_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            // get the column to sort by
+            Session["SortColumn"] = e.SortExpression;
+
+            // refresh the grid
+            this.GetStudents();
+
+            //toggle the sort direction
+            Session["SortDirection"] = Session["SortDirection"].ToString() == "ASC" ? "DESC" : "ASC";
+        }
+
+        protected void StudentsGridView_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (IsPostBack)
+            {
+                if (e.Row.RowType == DataControlRowType.Header) // if the row clicked is the header row
+                {
+                    LinkButton linkButton = new LinkButton();
+
+                    for (int index = 0; index < StudentsGridView.Columns.Count; index++) // check each column for a click
+                    {
+                        if (StudentsGridView.Columns[index].SortExpression == Session["SortColumn"].ToString())
+                        {
+                            if (Session["SortDirection"].ToString() == "ASC")
+                            {
+                                linkButton.Text = " <i class='fa fa-caret-up fa-lg'></i>";
+                            }
+                            else
+                            {
+                                linkButton.Text = " <i class='fa fa-caret-down fa-lg'></i>";
+                            }
+
+                            e.Row.Cells[index].Controls.Add(linkButton); // add the new linkbutton to header cell
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
